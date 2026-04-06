f8bec485bf501fe00317a272a9f49da08aa6b6be
mokhc <mokhonchuen@gmail.com>
2026-04-06 19:23:01 +0800

    Refactor comments and formatting in Binary Tree 1.py

--- Binary Tree 1.py
+++ Binary Tree 1.py
@@ -26,4 +26,4 @@ _4   5  6
 8
 """
 # reference :
-# https://weread.qq.com/web/reader/79c32c30727ecf0e79c7ef2k03a32a4023f03afdbd66a39
+# https://weread.qq.com/web/reader/79c32c30727ecf0e79c7ef2k44f328c023e44f683a8420b
